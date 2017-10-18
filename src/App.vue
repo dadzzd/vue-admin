d0ada4e722c94c513f4406731259817e2a316582
JOON YOUNG KIM <kjy@JOONui-MacBook-Pro.local>
2017-10-18 21:08:14 +0900

    add AppHeader,AppContent,Sidebar in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
+    <app-header></app-header>
+    <sidebar></sidebar>
+    <app-content></app-content>
     <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import AppFooter from './layout/AppFooter.vue'
-
+  import AppHeader from './layout/AppHeader.vue'
+  import Sidebar from './layout/Sidebar.vue'
+  import AppContent from './layout/AppContent.vue'
+  import AppFooter from './layout/AppFooter.vue'
 export default {
-  name: 'app',
-  components: {
-    AppFooter
+    name: 'app',
+    components: {
+      AppHeader,
+      Sidebar,
+      AppContent,
+      AppFooter
+    }
   }
-}
 </script>
 
 <style>
