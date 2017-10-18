1861d95fd719a0246307c4f9c0f71c3e0d0ce3e5
JOON YOUNG KIM <kjy@JOONui-MacBook-Pro.local>
2017-10-18 20:44:45 +0900

    add layout/AppFooter.vue && setting

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
