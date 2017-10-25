53c448b06d6c47c9679b3970d8ba6ffc0a69d06b
hattaehattae.cocoa@gmail.com <tol90779>
2017-10-25 20:51:58 +0900

    add user/index.vue

---
+++ src/views/user/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="title is-ancestor">
+    <div class="tile is-parent">
+      <div class="tile is-child box">
+        <div class="title">
+          user list
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'UserIndex'
+  }
+</script>
+
+<style>
+
+</style>
