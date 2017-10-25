bbbb07a160597621d351c012eaa839f308615989
hattaehattae.cocoa@gmail.com <tol90779>
2017-10-25 21:42:05 +0900

    pagination

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="title is-ancestor">
+  <div class="tile is-ancestor">
     <div class="tile is-parent">
       <div class="tile is-child box">
         <div class="title">
           user list
         </div>
         <vuetable
-        apiUrl="https://vuetable.ratiw.net/api/users"
-        :fields="['name', 'email', 'birthdate', 'salary']"></vuetable>
+        api-url="https://vuetable.ratiw.net/api/users"
+        :fields="['name', 'email', 'birthdate', 'salary']"
+        ref="vuetable"
+        pagination-path=""
+        @vuetable:pagination-data="onPaginationData"></vuetable>
+        <user-pagination
+          @vuetable-pagination:change-page="onChangePage"
+        ref="pagination"></user-pagination>
       </div>
     </div>
   </div>
@@ -15,10 +21,21 @@
 
 <script>
   import Vuetable from 'vuetable-2/src/components/Vuetable.vue'
+  import UserPagination from './components/UserPagination.vue'
+
   export default {
     name: 'UserIndex',
+    methods: {
+      onChangePage (page) {
+        this.$refs.vuetable.changePage(page)
+      },
+      onPaginationData (paginationData) {
+        this.$refs.pagination.setPaginationData(paginationData)
+      }
+    },
     components: {
-      Vuetable
+      Vuetable,
+      UserPagination
     }
   }
 </script>
