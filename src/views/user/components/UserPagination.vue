2d29daf77c5af0a9c6337606b1397e132bf1643c
hattaehattae.cocoa@gmail.com <tol90779>
2017-10-25 21:41:18 +0900

    add user/components/UserPagination.vue

---
+++ src/views/user/components/UserPagination.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav v-if="tablePagination && tablePagination.last_page > 1"
+  :class="pagination">
+    <a
+      @click="loadPage('prev')"
+      :class="['pagination-previous', isOnFirstPage ? 'is-disabled' : '']">
+      Prev
+    </a>
+    <a
+      @click="loadPage('next')"
+      :class="['pagination-next', isOnLastPage ? 'is-disabled' : '']">
+      Next
+    </a>
+
+    <ul class="pagination-list">
+        <li v-for="index in totalPage">
+          <a
+            @click="loadPage(index)"
+            :class="['pagination-link', isCurrentPage(index) ? 'is-current' : '']">
+            {{ index }}
+          </a>
+        </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  import PaginationMixin from 'vuetable-2/src/components/VuetablePaginationMixin.vue'
+  export default {
+    name: 'UserPagination',
+    mixins: [PaginationMixin]
+  }
+</script>
+
+<style>
+
+</style>
