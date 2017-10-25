89a72524ffb13aee1e62f2239448aaf6790727a0
hattaehattae.cocoa@gmail.com <tol90779>
2017-10-25 22:03:53 +0900

    add user/components/FilterBar.vue

---
+++ src/views/user/components/FilterBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="filter-bar control is-grouped">
+    <p class="control is-expanded has-addons">
+      <input
+        id="search" class="input"
+        type="text" v-model="filterText"
+        @keyup.enter="doFilter"
+        placeholder="write name or email">
+      <a @click="doFilter" class="button is-primary">search</a>
+    </p>
+    <p class="control">
+      <a @click="resetFilter">reset</a>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FilterBar',
+    data () {
+      return {
+        filterText: ''
+      }
+    },
+    methods: {
+      doFilter () {
+        this.$events.fire('filter-set', this.filterText)
+      },
+      resetFilter () {
+        this.filterText = ''
+        this.$events.fire('filter-reset')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #search {
+    width: 300px;
+  }
+</style>
